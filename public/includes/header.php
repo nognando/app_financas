e574bf4f41d7dea117a51518b0650c264a64c768
Fernando Aoki Nogueira <nando_tecnologia@hotmail.com>
2026-04-05 23:04:55 -0300

    php echo time no css header

--- public/includes/header.php
+++ public/includes/header.php
@@ -9,7 +9,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="css/style.css">
+    <!--<link rel="stylesheet" href="css/style.css">-->
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
 <body id="home">
 
