11f9711ca4c3056493dd1cafc35a08f7e3f6b4c7
Fernando Aoki Nogueira <nando_tecnologia@hotmail.com>
2026-04-06 22:19:11 -0300

    incluir font robot

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,8 +7,9 @@
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet">
-    
+    <!--<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&display=swap" rel="stylesheet">-->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+
     <!--<link rel="stylesheet" href="css/style.css">-->
     <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
 </head>
